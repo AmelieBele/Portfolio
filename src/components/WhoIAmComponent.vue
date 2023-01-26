40ee392a26edd9baba7816dd3ca49de76a8bd477
AmelieBele <amelie.benoist@icloud.com>
2023-01-26 17:23:06 +0100

    error

--- src/components/WhoIAmComponent.vue
+++ src/components/WhoIAmComponent.vue
@@ -4,7 +4,7 @@
         <p>
             Je suis Amélie, développeur web junior. J'ai, juste après mon bac, décider d'arrêter mes études et me suis dirigé vers le commerce 
             puis-qu'une occasion s'est présentée à moi. Dans le prêt-à-porter puis dans l'alimentaire, j'ai fini par en avoir assez vu et 
-            j'ai donc décidé de reprendre mes études. Ayant toujours été plutôt à l'aise avec l'outil informatique ,j'ai décidé passer ma formation de 
+            j'ai donc décidé de reprendre mes études. Ayant toujours été plutôt à l'aise avec l'outil informatique ,j'ai décidé de passer un formation de 
             développeur web avec Openclassrooms et l'ai obtenue avec succès. Il est temps pour moi d'entrer dans le monde du travail avec ce 
             nouveau diplôme en poche et de trouver l'employeur qui me laissera ma chance. J'ai hâte d'en apprendre plus sur ce travail
             qui regorge de choses que je ne connais pas encore !<span>;</span>
